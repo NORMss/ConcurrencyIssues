df2b64797c78f52c97c9600510405f0a3e94e54a
NORMss <serega.b301@gmail.com>
2024-12-13 00:20:29 +0700

    ref: refactoring the PassMeSomeWater solution

--- src/main/kotlin/problemPassMeSomeWater/solution.kt
+++ src/main/kotlin/problemPassMeSomeWater/solution.kt
@@ -6,34 +6,19 @@ import kotlin.concurrent.thread
 fun solution(): WaterBottle? {
     var bottle: WaterBottle? = null
 
-    val lock1 = WaterBottle.Karachinskaya
-    val lock2 = WaterBottle.Borjomi
-
     val firstWorker = thread {
-        val (firstLock, secondLock) = if (System.identityHashCode(lock1) < System.identityHashCode(lock2)) {
-            lock1 to lock2
-        } else {
-            lock2 to lock1
-        }
-
-        synchronized(firstLock) {
+        synchronized(WaterBottle.Karachinskaya) {
             Thread.sleep(1_000)
-            synchronized(secondLock) {
+            synchronized(WaterBottle.Borjomi) {
                 bottle = WaterBottle.Karachinskaya
             }
         }
     }
 
     val secondWorker = thread {
-        val (firstLock, secondLock) = if (System.identityHashCode(lock1) < System.identityHashCode(lock2)) {
-            lock1 to lock2
-        } else {
-            lock2 to lock1
-        }
-
-        synchronized(firstLock) {
+        synchronized(WaterBottle.Karachinskaya) {
             Thread.sleep(1_000)
-            synchronized(secondLock) {
+            synchronized(WaterBottle.Borjomi) {
                 bottle = WaterBottle.Borjomi
             }
         }
